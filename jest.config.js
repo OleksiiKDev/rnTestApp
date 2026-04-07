5a6e1a0006facd343e37449dbc746b17a5390d08
Oleksii Kharenko <oleksii.k@code-care.pro>
2026-04-07 11:24:09 +0300

    feat: add @ alias for src path and add navigation modules

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation|react-native-screens|react-native-safe-area-context)/)',
+  ],
 };
