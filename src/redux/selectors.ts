c6c8bc1bc586ed3a71c8321be404056038eaf6bc
Oleksii Kharenko <oleksii.k@code-care.pro>
2026-04-08 13:20:36 +0300

    feat: add details page

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -1,7 +1,15 @@
 import { RootState } from './store';
 import { createSelector } from '@reduxjs/toolkit';
 
+export const peopleState = (state: RootState) => state.people;
+
 export const peopleList = createSelector(
-  (state: RootState) => state.people,
+  peopleState,
   people => people.people,
 );
+
+export const personById = (id: string) =>
+  createSelector(
+    peopleState,
+    people => people.people.find(person => person.id === id),
+  );
--- src/screens/app/details/index.tsx
+++ src/screens/app/details/index.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import { useRoute } from '@react-navigation/native';
 import { DetailsRouteProp } from '@/types/navigation';
 import { View, Text } from 'react-native';
+import { useAppSelector } from "@/redux/hooks";
+import { personById } from "@/redux/selectors";
 
 const DetailsScreen = () => {
   const route = useRoute<DetailsRouteProp>();
   const personId = route.params?.personId ?? '';
+  const person = useAppSelector(personById(personId));
 
   return (
     <View>
-        <Text>Details Screen</Text>
-        <Text>{personId}</Text>
+        {person && (
+          <View>
+            <Text>{person.name}</Text>
+            <Text>{person.gender}</Text>
+            <Text>{person.birth_year}</Text>
+          </View>
+        )}
     </View>
   );
 }
