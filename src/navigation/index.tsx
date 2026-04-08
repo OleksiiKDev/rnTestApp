774279884f899212da089e1c239ae36477064de9
Oleksii Kharenko <oleksii.k@code-care.pro>
2026-04-08 07:29:22 +0300

    feat: add details screen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,15 +2,16 @@
 import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ROUTES } from '@/constants/routes';
-import  HomeScreen from '../screens/app/home';
+import  HomeScreen from '@/screens/app/home';
 import ListScreen from '@/screens/app/list';
+import DetailsScreen from '@/screens/app/details';
 
 const RootStack = createNativeStackNavigator({
   initialRouteName: ROUTES.HOME,
   screens: {
     [ROUTES.HOME]: HomeScreen,
     [ROUTES.LIST]: ListScreen,
-
+    [ROUTES.DETAILS]: DetailsScreen,
   },
 });
 
