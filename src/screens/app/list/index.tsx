3647161173de9f404cfa7bb67bab03b8d0394873
Oleksii Kharenko <oleksii.k@code-care.pro>
2026-04-08 10:19:57 +0300

    feat: add basic styling for list

--- src/screens/app/list/index.tsx
+++ src/screens/app/list/index.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import { fetchList } from '@/api/fetch-list';
 import { Person } from '@/types/card';
 
 const ListScreen = () => {
   const [list, setList] = useState<Person[]>([]);
 
-  
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -16,20 +15,20 @@ const ListScreen = () => {
         console.error('Error fetching list:', error);
       }
     };
-    
+
     fetchData();
   }, []);
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>List Screen</Text>
       <ScrollView>
         {list.map((person, index) => (
-          <View key={index}>
-            <Text>{person.name}</Text>
+          <View key={index} style={styles.item}>
+            <Text style={styles.name}>{person.name}</Text>
             <View>
-              <Text>{person.gender}</Text>
-              <Text>{person.birth_year}</Text>
+              <Text style={styles.details}>{person.gender}</Text>
+              <Text style={styles.details}>{person.birth_year}</Text>
             </View>
           </View>
         ))}
@@ -37,5 +36,25 @@ const ListScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  item: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  details: {
+    fontSize: 14,
+    color: '#666',
+  },
+});
 
 export default ListScreen;
